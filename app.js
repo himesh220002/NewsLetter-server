6b8010fa4762be0169a5fd325d02b261ea27350e
distance/ time <satyamhimesh@gmail.com>
2023-05-24 19:46:29 +0530

    ad

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post("/", function(req, res){
     const url = "https://us12.api.mailchimp.com/3.0/lists/e4cacb8c6e"
     const options = {
         method: "POST",
-        auth: "Himesh:857385ef2bc9cf8c756307cb1bb6c996-us12"
+        auth: "Himesh:b4231f32a2f7bf0c5556c56aad10558e-us12"
     }
     const request = https.request(url, options , function(response){
 
@@ -71,5 +71,5 @@ app.listen(process.env.PORT || 3000, function(){
     console.log("server is running on port : "+process.env.PORT)
 });
 
-//api key - 857385ef2bc9cf8c756307cb1bb6c996-us12
+//api key - b4231f32a2f7bf0c5556c56aad10558e-us12
 // audience key - e4cacb8c6e
